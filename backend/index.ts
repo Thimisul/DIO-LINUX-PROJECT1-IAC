121ac57d1aae70407858945c7dbc6803fc26cbeb
thimisul <thimisull@gmail.com>
2022-10-15 18:22:06 -0300

    api

--- backend/index.ts
+++ backend/index.ts
@@ -1,49 +1,66 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mysql from 'mysql2';
-var cors = require('cors')
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
+var cors = require("cors");
 
 type loginType = {
-  username: String,
-  password: String
-}
+  username: String;
+  password: String;
+};
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
-const pool =  mysql.createPool({
-  host: 'mysql',
-  user: 'root',
-  password: '123456',
-  database: 'LOGIN_TEST'
+const pool = mysql.createPool({
+  host: "mysql",
+  user: "root",
+  password: "123456",
+  database: "LOGIN_TEST",
 });
 
 app.use(bodyParser.json());
 
-app.post('/cadastro', function(req, res) {
-  var {username, password} = req.body
-  pool.promise().query('INSERT INTO users (username, password) VALUES(?,?)', [username, password])
-  .then(()=>{
-    res.status(201)
-  })
-  .catch(()=>{
-    res.status(500)
-  })
+app.post("/cadastro", function (req, res) {
+  var { username, password } = req.body;
+  pool
+    .promise()
+    .query("INSERT INTO users (username, password) VALUES(?,?)", [
+      username,
+      password,
+    ])
+    .then(() => {
+      res
+        .status(201)
+        .send(JSON.stringify({ username: username, password: password }));
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .send(JSON.stringify({ username: username, password: password }));
+    });
 });
 
-app.post('/login', async function(req, res) {
-  console.log(req.body)
-  var {username, password} = req.body
-  const [rows, fields] = await pool.promise().query( 'SELECT * FROM users WHERE username = ? and password = ?', [username, password], )
-    console.log(rows)
-    if(rows.toString().length > 0){
-      res.status(200)
-    }else{
-      res.status(404);
-    }
+app.post("/login", async function (req, res) {
+  console.log(req.body);
+  var { username, password } = req.body;
+  const [rows, fields] = await pool
+    .promise()
+    .query("SELECT * FROM users WHERE username = ? and password = ?", [
+      username,
+      password,
+    ]);
+  console.log(rows);
+  if (rows.toString().length > 0) {
+    res
+      .status(200)
+      .send(JSON.stringify({ username: username, password: password }));
+  } else {
+    res
+      .status(404)
+      .send(JSON.stringify({ username: username, password: password }));
   }
-);
+});
 
-app.listen(8000, function() {
-  console.log('Servidor rodando na porta 8000.');
+app.listen(8000, function () {
+  console.log("Servidor rodando na porta 8000.");
 });
